09d5bae9d1bb3a040fc7e62fbf03362bf32a423f
Gowtham Munukutla <gowtham.m81197@gmail.com>
2020-03-08 12:00:32 +0530

    ssl implemented but getting a tls handshake error
    Signed-off-by: Gowtham Munukutla <gowtham.m81197@gmail.com>

--- main.go
+++ main.go
@@ -4,22 +4,17 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"time"
 	"uuid-fy/api"
 )
 
 func main() {
 	r := mux.NewRouter()
 	
-	go func() {
-		time.Sleep()
-	}()
-	
 	r.Handle("/users/create", api.IsAuthorized(api.CreatePerson)).Methods("POST")
 	r.Handle("/users/update", api.IsAuthorized(api.UpdatePerson)).Methods("POST")
 	r.Handle("/users", api.IsAuthorized(api.GetPerson)).Methods("GET")
-	r.Handle("/getall", api.IsAuthorized(api.GetAllUsers)).Methods("GET")
+	r.HandleFunc("/getall", api.GetAllUsers).Methods("GET")
 	r.HandleFunc("/signin", api.Signin).Methods("POST")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServeTLS(":8000", "server.crt", "server.key", r))
 	
 }
